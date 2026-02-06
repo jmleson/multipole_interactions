f98f2dcc350d30eae62c1c477dc9b4787da10046
Judith <judith.leson@stud.uni-due.de>
2026-02-06 10:35:21 +0100

    MultipoleTerm: order and R-prefactor

--- SummandTerm.py
+++ SummandTerm.py
@@ -23,7 +23,9 @@ class MultipoleInteraction:
             term.factors.append( MultipoleMoment(greek_names[multipole_order_1:multipole_order_1+multipole_order_2]) )
             self.tensor_terms.append(term)
 
+        # initial values (! get changed by clean up):
         self.prefactor_expansion = 1/prefactor_expansion(order=self.order-1)
+        self.set_r_prefactor()
 
     def set_for_testing(self, tensor_terms, order):
         self.multipole1 = None
@@ -31,10 +33,9 @@ class MultipoleInteraction:
         self.order = order
         self.tensor_terms = tensor_terms
 
-    def get_r_prefactor(self):
-        r_prefactor = R("z")
-        r_prefactor.exponent = - (2 * self.order - 1)
-        return r_prefactor
+    def set_r_prefactor(self):
+        self.r_prefactor = R("z")
+        self.r_prefactor.exponent = - (2 * self.order + 1)
 
     def get_name(self):
         string = "T_" + "".join([pretty(i) for i in global_symbols[:self.order]])
@@ -44,17 +45,17 @@ class MultipoleInteraction:
     def full_string_tensor(self):
         string = self.get_name() + " = "
         if len(self.tensor_terms) > 0 and self.prefactor_expansion != 0:
+            if self.r_prefactor.exponent != 0:
+                string += f"{self.r_prefactor.to_string()} * "
             if self.prefactor_expansion != 1:
-                string += f"{self.get_r_prefactor().to_string()} * {self.prefactor_expansion} * [ "
-            else:
-                string += f"{self.get_r_prefactor().to_string()} * [ "
+                string += f"{self.prefactor_expansion} * "
+            string += f" [ "
             string += " ".join([term.to_string() for term in self.tensor_terms if not term.prefactor == 0 ])
             string += " ]"
         else:
             string += "+ 0"
 
         return string
-
 
 
     def add_up(self):
@@ -75,6 +76,14 @@ class MultipoleInteraction:
             for term in self.tensor_terms:
                 term.prefactor *= self.prefactor_expansion
         self.prefactor_expansion = 1
+
+        if self.r_prefactor.exponent != 0:
+            for term in self.tensor_terms:
+                r = R("z")
+                r.exponent = self.r_prefactor.exponent
+                term.factors.append(r)
+                term.clean_up()
+            self.r_prefactor.exponent = 0
 
     def simplify(self):
         for term in self.tensor_terms:
---
+++ testing/testing_MultipoleInteractionTerm.py
@@ -0,0 +1,34 @@
+import unittest
+
+from SummandTerm import MultipoleInteraction
+
+
+class TestMultipoleInteractionTerm(unittest.TestCase):
+
+
+    def test_set_r_prefactor(self):
+        s = MultipoleInteraction(multipole_order_1=1, multipole_order_2=1)
+        assert s.order == 2
+        assert s.r_prefactor.exponent == -5
+
+        s = MultipoleInteraction(multipole_order_1=1, multipole_order_2=2)
+        assert s.order == 3
+        assert s.r_prefactor.exponent == -7
+
+        s = MultipoleInteraction(multipole_order_1=1, multipole_order_2=3)
+        assert s.order == 4
+        assert s.r_prefactor.exponent == -9
+
+
+    def test_get_name(self):
+        s = MultipoleInteraction(multipole_order_1=1, multipole_order_2=1)
+        assert s.get_name() == "T_αβ μ_α μ_β"
+
+        s = MultipoleInteraction(multipole_order_1=1, multipole_order_2=2)
+        assert s.get_name() == "T_αβγ μ_α Θ_βγ"
+
+        s = MultipoleInteraction(multipole_order_1=2, multipole_order_2=1)
+        assert s.get_name() == "T_αβγ Θ_αβ μ_γ"
+
+        s = MultipoleInteraction(multipole_order_1=1, multipole_order_2=3)
+        assert s.get_name() == "T_αβγδ μ_α Ω_βγδ"
